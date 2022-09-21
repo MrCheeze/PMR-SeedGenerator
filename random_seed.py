229335db4793e6a6a688757eb64f478fda1b088e
JCog <jordancogan4@gmail.com>
2022-09-20 22:32:43 -0500

    create race mode with variable number of required chapters

--- random_seed.py
+++ random_seed.py
@@ -14,7 +14,8 @@ from rando_modules.modify_entrances import \
     get_bowsercastle_bossrush,\
     get_gear_location_shuffle,\
     get_glitched_logic,\
-    adjust_rip_cheato_pricing
+    adjust_rip_cheato_pricing, \
+    get_race_mode
 from rando_modules.random_formations import get_random_formations
 from rando_modules.random_movecosts import get_randomized_moves
 from rando_modules.random_mystery import get_random_mystery
@@ -78,6 +79,17 @@ class RandomSeed:
             self.rando_settings.ripcheato_items_in_logic
         )
         world_graph = get_glitched_logic(world_graph, self.rando_settings.glitch_settings, self.rando_settings.bowsers_castle_mode["value"])
+
+        # Race Mode
+        # TODO: create a proper setting for this
+        race_chapters_not_required = [1, 2, 3, 4, 5, 6, 7]
+        race_chapters_required = []
+        for i in range(0, self.rando_settings.starway_spirits_needed["value"]):
+            chapter_index = random.randrange(0, len(race_chapters_not_required))
+            race_chapters_required.append(race_chapters_not_required.pop(chapter_index))
+        race_chapters_required.sort()
+        print("Race Mode Chapters: " + str(race_chapters_required))
+        world_graph = get_race_mode(world_graph, race_chapters_not_required, self.rando_settings.starway_spirits_needed["value"])
 
         hidden_block_mode = self.rando_settings.hidden_block_mode["value"]
         if self.rando_settings.glitch_settings.knows_hidden_blocks["value"]:
